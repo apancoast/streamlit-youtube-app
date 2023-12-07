26cf2349fc8a17ef8dc46cc358fb613b1dc924ac
apancoast <98405259+apancoast@users.noreply.github.com>
2023-12-07 11:14:48 -0500

    Update top_youtube_channels_app.py
    Display top 5

--- top_youtube_channels_app.py
+++ top_youtube_channels_app.py
@@ -1,18 +1,37 @@
-import streamlit as st
-import pandas as pd
 import json
-import requests
-import re
-from PIL import Image, ImageFont, ImageDraw
-from io import BytesIO
-import os
+import pandas as pd
+import streamlit as st
 
-# Function to process the uploaded file
+def extract_data_from_element(element):
+    og_title = element['title']
+    datetime = element['time']
+    
+    if og_title == 'Answered survey question':
+        return og_title, 'From Google Ads', None, None, None, None
+    
+    if og_title == 'Watched a video that has been removed':
+        return og_title, 'No', None, None, None, None
+    
+    if og_title.startswith('Visited'):
+        return 'Visted Ad', 'From Google Ads', None, None, None, None
+    
+    if og_title.startswith('Watched'):
+        title = og_title[len('Watched '):]
+        vid_link = element['titleUrl']
+        ad = 'From Google Ads' if 'details' in element else 'No'
+        
+        subtitles = element.get('subtitles', [])
+        if subtitles:
+            subtitle = subtitles[0]
+            channel = subtitle.get('name')
+            channel_link = subtitle.get('url')
+            return title, ad, vid_link, channel, channel_link, datetime
+    
+    return None, None, None, None, None, None
+
 def process_file(uploaded_file):
-    # Read the uploaded file
     file_contents = uploaded_file.read()
 
-    # Initialize empty lists to store extracted data
     titles = []
     vid_links = []
     channels = []
@@ -20,58 +39,30 @@ def process_file(uploaded_file):
     datetimes = []
     ads = []
 
-    # Retrieve data from JSON
     json_data = json.loads(file_contents)
     for element in json_data:
-        datetime = element['time']
-        if datetime.startswith('2023'):
-            og_title = element['title']
-            title = None
-            vid_link = None
-            channel = None
-            channel_link = None
-            ad = None
+        if element['time'].startswith('2023'):
+            (
+                title,
+                ad,
+                vid_link,
+                channel,
+                channel_link,
+                datetime
+            ) = extract_data_from_element(element)
             
-            if og_title == 'Answered survey question':
-                title = og_title
-                ad = 'From Google Ads'
-                
-            elif og_title == 'Watched a video that has been removed':
-                title = og_title
-                ad = 'No'
-            
-            elif og_title.startswith('Visited'):
-                title = 'Visted Ad'
-                ad = 'From Google Ads'
-            
-            elif og_title.startswith('Watched'):
-                title = og_title[len('Watched '):]
-                vid_link = element['titleUrl']
-                ad = 'From Google Ads' if 'details' in element else 'No'
-                
-                subtitles = element.get('subtitles', [])
-                if subtitles:
-                    subtitle = subtitles[0]
-                    channel = subtitle.get('name')
-                    channel_link = subtitle.get('url')
-                else:
-                    continue
-                
-            else:
+            if title is None:
                 continue
             
+            titles.append(title)
+            vid_links.append(vid_link)
+            channels.append(channel)
+            channel_links.append(channel_link)
+            datetimes.append(datetime)
+            ads.append(ad)
         else:
             break
 
-        # Append extracted data to respective lists
-        titles.append(title)
-        vid_links.append(vid_link)
-        channels.append(channel)
-        channel_links.append(channel_link)
-        datetimes.append(datetime)
-        ads.append(ad)
-
-    # Create a Pandas DataFrame from the extracted data
     data = {
         'title': titles,
         'vid_url': vid_links,
@@ -83,14 +74,20 @@ def process_file(uploaded_file):
     history_df = pd.DataFrame(data)
     return history_df
 
-# Streamlit app
 def main():
     st.title("YouTube History Parser")
     uploaded_file = st.file_uploader("Upload a JSON file", type=['json'])
 
     if uploaded_file is not None:
-        df = process_file(uploaded_file)
-        st.dataframe(df.head())
+        try:
+            history_df = process_file(uploaded_file)
+            no_dups = history_df.drop_duplicates(subset='vid_url')
+            no_ads = no_dups[no_dups['ad'] == 'No']
+            top_5_df = no_ads['channel'].value_counts().head(5).reset_index().rename(columns={'index': 'channel', 'channel': 'counts'})
+            top_5_df = pd.merge(top_5_df, history_df[['channel', 'channel_url']], on='channel', how='left').drop_duplicates().reset_index(drop=True)
+            st.dataframe(top_5_df)
+        except (json.JSONDecodeError, KeyError) as e:
+            st.error("Error processing the file. Please make sure it's a valid JSON file.")
 
 if __name__ == "__main__":
     main()
