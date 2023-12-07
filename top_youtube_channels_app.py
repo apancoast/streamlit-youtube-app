8bcd1c42ea58e1f79491204d19a8ae9b47c14aaf
apancoast <98405259+apancoast@users.noreply.github.com>
2023-12-07 16:17:08 -0500

    Update top_youtube_channels_app.py

--- top_youtube_channels_app.py
+++ top_youtube_channels_app.py
@@ -94,7 +94,7 @@ def process_file(uploaded_file):
     top_5_df = no_ads.channel.value_counts().sort_values(ascending=False).head(5).reset_index().rename(columns={'index': 'channel', 'channel': 'counts'})
     
     # Get channel links so we can get profile pictures
-    top_5_df = pd.merge(top_5_df, st.session_state.history_df, on='channel', how='left').drop_duplicates().reset_index(drop=True)
+    top_5_df = pd.merge(top_5_df, history_df, on='channel', how='left').drop_duplicates().reset_index(drop=True)
     return top_5_df
 
 def create_image(background_image, dataframe):
